6c6f22016aa306f4254a384986609b08fca4149f
emokid2008 <karta7756@gmail.com>
2026-04-13 12:51:24 +0300

    edit dataset

--- AI/AI_model.py
+++ AI/AI_model.py
@@ -1,1 +1,23 @@
 #init
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from keras.utils import to_categorical
+import pickle 
+
+data_frame = pd.read_csv('datasset_generator.py')
+
+data = data_frame['main'].values
+target = data_frame['clothing_advice'].values
+
+vectorizer = TfidfVectorizer(max_features = 1000)
+
+data_scaled = vectorizer.fit_transform(data).toarray()
+
+data_study, data_test, target_study, target_test = train_test_split(data_scaled, target, test_size = 0.2, random_state = 42, stratify = target)
+
+target_study_cat = to_categorical(target_study, num_classes = 10)
+target_test_cat = to_categorical(target_test, num_classes = 10)
+
+with open ('vectorizer.pkl', 'wb') as file:
+    pickle.dump(vectorizer, file)
