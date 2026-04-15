2958eb05729c783993a7eef7bae09c3ceab7bee6
emokid2008 <karta7756@gmail.com>
2026-04-15 13:28:01 +0300

    rename

---
+++ AI/AI_model_prepare.py
@@ -0,0 +1,27 @@
+#init
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from keras.utils import to_categorical
+import pickle 
+import numpy as np
+
+
+data_frame = pd.read_csv('dataset.csv')
+print(data_frame.keys())
+
+data = data_frame.drop(columns = ['main','clothing_advice','label']).values
+target = data_frame['label'].values
+print(target)
+vectorizer = TfidfVectorizer(max_features = 1000)
+
+data_scaled = vectorizer.fit_transform(data_frame['main']).toarray()
+data = np.hstack((data_scaled, data))
+
+data_study, data_test, target_study, target_test = train_test_split(data_scaled, target, test_size = 0.2, random_state = 42, stratify = target)
+
+target_study_cat = to_categorical(target_study, num_classes = 12)
+target_test_cat = to_categorical(target_test, num_classes = 12)
+
+with open ('vectorizer.pkl', 'wb') as file:
+    pickle.dump(vectorizer, file)
