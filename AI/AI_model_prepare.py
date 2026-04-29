a4ca5c65fd447ce1815d7c5820ec632e41ba1c7f
emokid2008 <karta7756@gmail.com>
2026-04-29 13:03:42 +0300

    add scaler

--- AI/AI_model_prepare.py
+++ AI/AI_model_prepare.py
@@ -1,5 +1,6 @@
 #init
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from keras.utils import to_categorical
@@ -17,8 +18,15 @@ vectorizer = TfidfVectorizer(max_features = 1000)
 
 data_scaled = vectorizer.fit_transform(data_frame['main']).toarray()
 data = np.hstack((data_scaled, data))
+print(data_scaled)
+print(data)
 
-data_study, data_test, target_study, target_test = train_test_split(data_scaled, target, test_size = 0.2, random_state = 42, stratify = target)
+data_study, data_test, target_study, target_test = train_test_split(data, target, test_size = 0.2, random_state = 42, stratify = target)
+print(data_study.shape, target_study.shape)
+
+scaler = StandardScaler()
+data_study = scaler.fit_transform(data_study)
+data_test = scaler.transform(data_test)
 
 target_study_cat = to_categorical(target_study, num_classes = 12)
 target_test_cat = to_categorical(target_test, num_classes = 12)
