771ead9adbaecf24a7d210659d0f549accffe50a
emokid2008 <karta7756@gmail.com>
2026-05-06 13:01:12 +0300

    add ai_model

--- BOT/main_bot.py
+++ BOT/main_bot.py
@@ -1,8 +1,11 @@
 #init
+import pickle
+from keras.models import load_model
 import telebot
 bot = telebot.TeleBot('')
-
+model = load_model('./AI/Ai_model.h5')
+with open('vectorizer.pkl', 'rb') as file:
+    vectorizer = pickle.load(file)
 @bot.message_handler(commands = ['start'])
 def start(message):
     bot.send_message(message.chat.id, 'Доброго времени суток, я бот - прогноз погоды,\nА также я помогаю с выбором одежды.\nЧтобы начать укажите свой город.') 
-    
