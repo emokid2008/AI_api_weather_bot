8ba3f9729313673a3b7efaa71ebb710bb333d321
emokid2008 <karta7756@gmail.com>
2026-05-06 12:53:33 +0300

    start

--- BOT/main_bot.py
+++ BOT/main_bot.py
@@ -1,2 +1,8 @@
 #init
-#gsgsgsgs
+import telebot
+bot = telebot.TeleBot('')
+
+@bot.message_handler(commands = ['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Доброго времени суток, я бот - прогноз погоды,\nА также я помогаю с выбором одежды.\nЧтобы начать укажите свой город.') 
+    
